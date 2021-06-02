dc7c1fd694c93fd5a2c5f315082d58ac8221387b
Zaki <abdanzaki07@gmail.com>
2021-06-02 12:36:00 +0700

    menamahkan leak canary, obfuscation dengan ProGuard, encryption pada database, dan certificate pinning

--- core/src/main/java/com/zaki/cinemaapp/core/di/CoreModule.kt
+++ core/src/main/java/com/zaki/cinemaapp/core/di/CoreModule.kt
@@ -8,6 +8,9 @@ import com.zaki.cinemaapp.core.data.source.local.room.CinemaDatabase
 import com.zaki.cinemaapp.core.data.source.remote.RemoteDataSource
 import com.zaki.cinemaapp.core.domain.repository.ICinemaRepository
 import com.zaki.cinemaapp.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,19 +22,30 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<CinemaDatabase>().cinemaDao() }
     single {
+        val passPharse: ByteArray = SQLiteDatabase.getBytes("cinema".toCharArray())
+        val factory = SupportFactory(passPharse)
         Room.databaseBuilder(
             androidContext(),
             CinemaDatabase::class.java, "CinemaDb"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .add(hostname, "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
